a3e185a1ad4e797b67061e4aa923a8abf8084ead
StanislavMelnik1991 <StanislavMelnik@gmail.com>
2024-03-20 13:46:16 +0300

    fix: login btn

--- src/_entities/telegram/LoginButton/index.tsx
+++ src/_entities/telegram/LoginButton/index.tsx
@@ -7,16 +7,28 @@ interface Props {
 }
 
 export const LoginButton = ({ className }: Props) => {
-  // const url = `https://oauth.telegram.org/embed/${process.env.TG_BOT_NAME}?origin=${process.env.HOST}&request_access=write`;
+  const url = `https://oauth.telegram.org/embed/${process.env.TG_BOT_NAME}?origin=${process.env.HOST}&request_access=write`;
   return (
     <div className={classNames(styles.wrapper, className)}>
-      <script
-        async
-        src="https://telegram.org/js/telegram-widget.js?22"
-        data-telegram-login={process.env.TG_BOT_NAME}
-        data-size="medium"
-        data-userpic="false"
-        data-auth-url="https://admin.ship.cloudns.be/login"
+      <p>{`data-auth-url="${process.env.HOST}/login"`}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `<script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="${process.env.TG_BOT_NAME}" data-size="medium" data-auth-url="${process.env.HOST}/login" data-request-access="write"></script>`,
+        }}
+      />
+      <p>{'data-onauth="onTelegramAuth(user)"'}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `<script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="${process.env.TG_BOT_NAME}" data-size="medium" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>`,
+        }}
+      />
+      <p>iframe</p>
+      <iframe
+        src={url}
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        scrolling="no"
       />
     </div>
   );
