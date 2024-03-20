ea47653c90ce25c780f8a8403a5d7662e5a0bd11
StanislavMelnik1991 <StanislavMelnik@gmail.com>
2024-03-20 14:22:38 +0300

    test separate strings

--- src/_entities/telegram/LoginButton/index.tsx
+++ src/_entities/telegram/LoginButton/index.tsx
@@ -12,7 +12,14 @@ export const LoginButton = ({ className }: Props) => {
       <p>{`data-auth-url="${process.env.HOST}/login"`}</p>
       <div
         dangerouslySetInnerHTML={{
-          __html: `<script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="${process.env.TG_BOT_NAME}" data-size="medium" data-auth-url="login" data-request-access="write"></script>`,
+          __html: `<script async 
+          src="https://telegram.org/js/telegram-widget.js?22" 
+          data-telegram-login="${process.env.TG_BOT_NAME}"
+           data-userpic="false" 
+           data-radius="10" 
+           data-size="medium" 
+           data-auth-url="login"
+           data-request-access="write"></script>`,
         }}
       />
     </div>
--- src/_entities/telegram/onAuth/index.tsx
+++ src/_entities/telegram/onAuth/index.tsx
@@ -8,7 +8,21 @@ export const TelegramOnAuthButton = () => {
       <p>{'data-onauth="onTelegramAuth(user)"'}</p>
       <div
         dangerouslySetInnerHTML={{
-          __html: `<script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="${process.env.TG_BOT_NAME}" data-size="medium" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>`,
+          __html: `<script 
+          async 
+          src="https://telegram.org/js/telegram-widget.js?22" 
+          data-telegram-login="${process.env.TG_BOT_NAME}" 
+          data-size="medium" 
+          data-userpic="false"
+          data-radius="10"
+          data-onauth="onTelegramAuth(user)" 
+          data-request-access="write"></script>
+            <script type="text/javascript">
+              function onTelegramAuth(user) {
+                console.log(user)
+                alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+              }
+            </script>`,
         }}
       />
     </div>
